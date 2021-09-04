6b67b10cac177d55a5bb0b5d8a904f7dfaf65e9b
UPye <Urika.Pye@gmail.com>
2021-09-03 20:14:00 -0400

    I think I deployed it

---
+++ client/build/precache-manifest.5b325202172bc6bf277576e60cf6c93f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68deb2ebff28d29e3657c94f0e9bbf1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "650039bc5b2b7a732b2d",
+    "url": "/static/css/main.b6aebb93.chunk.css"
+  },
+  {
+    "revision": "b1591577a788e322a9f9",
+    "url": "/static/js/2.8f54fcf3.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.8f54fcf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650039bc5b2b7a732b2d",
+    "url": "/static/js/main.5ad7c829.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
